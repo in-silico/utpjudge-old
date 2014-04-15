d7a1b0d7efcddbf870998785fe2e6cc886c8fc7b
pin3da <manuel.felipe.pineda@gmail.com>
2014-04-15 00:24:15 -0500

    Example checker

---
+++ protected/checkers/float_diff.cpp
@@ -0,0 +1,34 @@
+using namespace std;
+#include<bits/stdc++.h>
+
+/**
+ * Example Checker for floating points answers.
+ * Params : Contestant.output, official.output, official.input
+ * Return :
+ *        0 -> Accepted.
+ *        1 -> Presentation error.
+ *        >= 2 -> Wrong Answer.
+ *
+ * Author : Manuel Felipe Pineda.
+ * Date   : 14 - 04 - 2014.
+ * */
+
+
+int main (int argc, char ** argv) {
+
+  if (argc < 3) return 5;
+
+  ifstream contestant (argv[1], ifstream::in);
+  ifstream official (argv[2], ifstream::in);
+
+  while (contestant.good()) {
+    if (!official.good()) return 5; // Printed extra data.
+    double a1; contestant>>a1;
+    double a2; official>>a2;
+    if (fabs(a1 - a2) > 1e-4) return 2;
+  }
+
+  if (official.good()) return 5; // Not enough data.
+
+  return 0; // All test passed.
+}
